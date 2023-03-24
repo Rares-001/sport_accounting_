4cf7e171d01204c767c24a4e5a082cb0ac9d36a6
Rares-001 <91318114+Rares-001@users.noreply.github.com>
2023-03-24 13:52:44 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-def create_app(config_name):
+def create_app(config=None):
     app = Flask(__name__)
 
-    app.config.from_object(config_name)
+    if config:
+        app.config.from_object(config)
+
+    # Set the database URI before initializing the app
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/sport_accounting'
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -19,6 +24,9 @@ def create_app(config_name):
     from app.routes.home import home as home_blueprint
     app.register_blueprint(home_blueprint)
 
+    # Fix for 403 error: add the following lines
+    CORS(app)
+
     return app
 
-from app import models
+
