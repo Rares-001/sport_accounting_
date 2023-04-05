abb4f61c833fc9a170c6f8d48642e0a174a6c3c0
kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2023-04-06 01:37:05 +0200

    updated login and registration

--- barmodule/asgi.py
+++ barmodule/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for barmodule project.
+ASGI config for barModule project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barmodule.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barModule.settings')
 
 application = get_asgi_application()
